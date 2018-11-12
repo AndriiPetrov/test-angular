d656c3cab210b135db32522a69109587650dd874
Andrii Petrov <apetrov@Andriis-iMac.local>
2018-11-12 19:13:33 +0200

    just some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-app';
+export class AppComponent implements OnInit {
+
+  user: User;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('user.json').subscribe((data: User) => this.user = data);
+  }
 }
